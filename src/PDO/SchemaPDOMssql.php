c2a8671596b116b2832b327e6fbc959f5c3af183
darkdef <darkdef@gmail.com>
2022-04-10 20:46:23 +0300

    Simplification of Connection - removing pool from Connection

--- src/PDO/SchemaPDOMssql.php
+++ src/PDO/SchemaPDOMssql.php
@@ -1070,7 +1070,7 @@ final class SchemaPDOMssql extends Schema implements ViewInterface
      */
     protected function normalizePdoRowKeyCase(array $row, bool $multiple): array
     {
-        if ($this->db->getSlavePdo()?->getAttribute(PDO::ATTR_CASE) !== PDO::CASE_UPPER) {
+        if ($this->db->getOpenPDO()?->getAttribute(PDO::ATTR_CASE) !== PDO::CASE_UPPER) {
             return $row;
         }
 
