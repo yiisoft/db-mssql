56fa1dfc35afcac0f1a6bac857790ff2af1f521d
darkdef <darkdef@gmail.com>
2023-01-21 22:31:00 +0300

    MSSQL migration alterColumn on a column with default value NULL creates wrong CONSTRAINT https://github.com/yiisoft/yii2/pull/18617

--- src/ColumnSchemaBuilder.php
+++ src/ColumnSchemaBuilder.php
@@ -4,8 +4,21 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Mssql;
 
+use Yiisoft\Db\Expression\Expression;
 use Yiisoft\Db\Schema\AbstractColumnSchemaBuilder;
 
 final class ColumnSchemaBuilder extends AbstractColumnSchemaBuilder
 {
+    /**
+     * Getting the `Default` value for constraint
+     * @return string|Expression|null
+     */
+    public function getDefault(): Expression|string|null
+    {
+        if ($this->default instanceof Expression) {
+            return $this->default;
+        }
+
+        return $this->buildDefaultValue();
+    }
 }
--- src/DDLQueryBuilder.php
+++ src/DDLQueryBuilder.php
@@ -75,7 +75,7 @@ final class DDLQueryBuilder extends AbstractDDLQueryBuilder
                     "DF_{$constraintBase}",
                     $table,
                     $column,
-                    $defaultValue
+                    $defaultValue instanceof Expression ? $defaultValue : new Expression($defaultValue)
                 );
             }
 
