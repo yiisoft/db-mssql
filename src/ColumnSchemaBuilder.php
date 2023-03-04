02fc9aa11ebfbdce8392f594e0934fed7df90f25
Wilmer Arambula <terabytesoftw@gmail.com>
2023-03-04 10:04:29 -0300

    Add phhpdoc `ColumnSchemaBuilder` class.

--- src/ColumnSchemaBuilder.php
+++ src/ColumnSchemaBuilder.php
@@ -7,12 +7,26 @@ namespace Yiisoft\Db\Mssql;
 use Yiisoft\Db\Expression\Expression;
 use Yiisoft\Db\Schema\AbstractColumnSchemaBuilder;
 
+/**
+ * It's a utility that provides a convenient way to create column schema for use with {@see `\Yiisoft\Db\Mssql\Schema`}
+ * for MSSQL Server.
+ *
+ * It provides methods for specifying the properties of a column, such as its type, size, default value, and whether it
+ * is nullable or not. It also provides a method for creating a column schema based on the specified properties.
+ *
+ * For example, the following code creates a column schema for an integer column:
+ *
+ * ```php
+ * $column = (new ColumnSchemaBuilder(SchemaInterface::TYPE_INTEGER))->notNull()->defaultValue(0);
+ * ```
+ *
+ * Provides a fluent interface, which means that the methods can be chained together to create a column schema with
+ * multiple properties in a single line of code.
+ */
 final class ColumnSchemaBuilder extends AbstractColumnSchemaBuilder
 {
     /**
-     * Getting the `Default` value for constraint
-     *
-     * @return Expression|string|null
+     * @return Expression|string|null The default value of the column
      */
     public function getDefault(): Expression|string|null
     {
