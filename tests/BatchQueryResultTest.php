b006d7d9279167042cdd3e285a0860ba02d1c37c
Valerii Gorbachev <darkdef@gmail.com>
2023-03-05 12:41:39 +0300

    BatchQueryResult mini refactoring (#222)
    * cov fix
    
    * styleci

--- tests/BatchQueryResultTest.php
+++ tests/BatchQueryResultTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Mssql\Tests;
 
+use Yiisoft\Db\Mssql\BatchQueryResult;
 use Yiisoft\Db\Mssql\Tests\Support\TestTrait;
+use Yiisoft\Db\Query\Query;
 use Yiisoft\Db\Tests\Common\CommonBatchQueryResultTest;
 
 /**
@@ -13,4 +15,21 @@ use Yiisoft\Db\Tests\Common\CommonBatchQueryResultTest;
 final class BatchQueryResultTest extends CommonBatchQueryResultTest
 {
     use TestTrait;
+
+    public function testBatchQueryResultWithoutPopulate(): void
+    {
+        $db = $this->getConnection(true);
+
+        $query = new Query($db);
+        $query->from('customer')->orderBy('id')->limit(3)->indexBy('id');
+
+        $batchQueryResult = new BatchQueryResult($query);
+
+        $customers = $this->getAllRowsFromBatch($batchQueryResult);
+
+        $this->assertCount(3, $customers);
+        $this->assertEquals('user1', $customers[0]['name']);
+        $this->assertEquals('user2', $customers[1]['name']);
+        $this->assertEquals('user3', $customers[2]['name']);
+    }
 }
