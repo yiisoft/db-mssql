813b0beccaeaab2bab05f82c8a690d8782aacf8e
Wilmer Arambula <terabytesoftw@gmail.com>
2023-03-08 13:13:14 -0300

    More clean code.

--- src/Schema.php
+++ src/Schema.php
@@ -158,7 +158,7 @@ final class Schema extends AbstractSchema
      * @throws InvalidConfigException
      * @throws Throwable
      *
-     * @return array All schemas name in the database, except system schemas.
+     * @return array All schema name in the database, except system schemas.
      *
      * @link https://docs.microsoft.com/en-us/sql/relational-databases/system-catalog-views/sys-database-principals-transact-sql
      */
