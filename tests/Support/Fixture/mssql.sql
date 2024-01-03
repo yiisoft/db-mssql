6b0d16f5bed461c49484b0927c6507202de4a25b
Tigrov <rrr-r@ya.ru>
2024-01-04 06:36:30 +0700

    Update fixtures according to tests

--- tests/Support/Fixture/mssql.sql
+++ tests/Support/Fixture/mssql.sql
@@ -145,18 +145,18 @@ CREATE TABLE [dbo].[negative_default_values] (
 
 CREATE TABLE [dbo].[type] (
     [int_col] [int] NOT NULL,
+    [float_col] [decimal](4,3) NOT NULL,
+    [char_col] [char](100) NOT NULL,
+    [bool_col] [bit] NOT NULL,
     [int_col2] [int] DEFAULT '1',
     [tinyint_col] [tinyint] DEFAULT '1',
     [smallint_col] [smallint] DEFAULT '1',
-    [char_col] [char](100) NOT NULL,
     [char_col2] [varchar](100) DEFAULT 'something',
     [char_col3] [text],
-    [float_col] [decimal](4,3) NOT NULL,
     [float_col2] [float] DEFAULT '1.23',
     [blob_col] [varbinary](MAX),
     [numeric_col] [decimal](5,2) DEFAULT '33.22',
     [datetime_col] [datetime] NOT NULL DEFAULT '2002-01-01 00:00:00',
-    [bool_col] [bit] NOT NULL,
     [bool_col2] [bit] DEFAULT '1'
 );
 
