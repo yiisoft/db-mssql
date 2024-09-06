ec9f24777d4f271df0d02536c100fb427e00b0c1
Tigrov <rrr-r@ya.ru>
2024-09-06 13:40:15 +0700

    Separate column type constants

--- src/Column/ColumnFactory.php
+++ src/Column/ColumnFactory.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Mssql\Column;
 
+use Yiisoft\Db\Constant\ColumnType;
 use Yiisoft\Db\Schema\Column\AbstractColumnFactory;
 use Yiisoft\Db\Schema\Column\ColumnSchemaInterface;
-use Yiisoft\Db\Schema\SchemaInterface;
 
 final class ColumnFactory extends AbstractColumnFactory
 {
@@ -17,63 +17,63 @@ final class ColumnFactory extends AbstractColumnFactory
      */
     private const TYPE_MAP = [
         /** Exact numbers */
-        'bit' => SchemaInterface::TYPE_BOOLEAN,
-        'tinyint' => SchemaInterface::TYPE_TINYINT,
-        'smallint' => SchemaInterface::TYPE_SMALLINT,
-        'int' => SchemaInterface::TYPE_INTEGER,
-        'bigint' => SchemaInterface::TYPE_BIGINT,
-        'numeric' => SchemaInterface::TYPE_DECIMAL,
-        'decimal' => SchemaInterface::TYPE_DECIMAL,
-        'smallmoney' => SchemaInterface::TYPE_MONEY,
-        'money' => SchemaInterface::TYPE_MONEY,
+        'bit' => ColumnType::BOOLEAN,
+        'tinyint' => ColumnType::TINYINT,
+        'smallint' => ColumnType::SMALLINT,
+        'int' => ColumnType::INTEGER,
+        'bigint' => ColumnType::BIGINT,
+        'numeric' => ColumnType::DECIMAL,
+        'decimal' => ColumnType::DECIMAL,
+        'smallmoney' => ColumnType::MONEY,
+        'money' => ColumnType::MONEY,
 
         /** Approximate numbers */
-        'float' => SchemaInterface::TYPE_FLOAT,
-        'real' => SchemaInterface::TYPE_FLOAT,
-        'double' => SchemaInterface::TYPE_DOUBLE,
+        'float' => ColumnType::FLOAT,
+        'real' => ColumnType::FLOAT,
+        'double' => ColumnType::DOUBLE,
 
         /** Date and time */
-        'date' => SchemaInterface::TYPE_DATE,
-        'time' => SchemaInterface::TYPE_TIME,
-        'smalldatetime' => SchemaInterface::TYPE_DATETIME,
-        'datetime' => SchemaInterface::TYPE_DATETIME,
-        'datetime2' => SchemaInterface::TYPE_DATETIME,
-        'datetimeoffset' => SchemaInterface::TYPE_DATETIME,
+        'date' => ColumnType::DATE,
+        'time' => ColumnType::TIME,
+        'smalldatetime' => ColumnType::DATETIME,
+        'datetime' => ColumnType::DATETIME,
+        'datetime2' => ColumnType::DATETIME,
+        'datetimeoffset' => ColumnType::DATETIME,
 
         /** Character strings */
-        'char' => SchemaInterface::TYPE_CHAR,
-        'varchar' => SchemaInterface::TYPE_STRING,
-        'text' => SchemaInterface::TYPE_TEXT,
+        'char' => ColumnType::CHAR,
+        'varchar' => ColumnType::STRING,
+        'text' => ColumnType::TEXT,
 
         /** Unicode character strings */
-        'nchar' => SchemaInterface::TYPE_CHAR,
-        'nvarchar' => SchemaInterface::TYPE_STRING,
-        'ntext' => SchemaInterface::TYPE_TEXT,
+        'nchar' => ColumnType::CHAR,
+        'nvarchar' => ColumnType::STRING,
+        'ntext' => ColumnType::TEXT,
 
         /** Binary strings */
-        'binary' => SchemaInterface::TYPE_BINARY,
-        'varbinary' => SchemaInterface::TYPE_BINARY,
-        'image' => SchemaInterface::TYPE_BINARY,
+        'binary' => ColumnType::BINARY,
+        'varbinary' => ColumnType::BINARY,
+        'image' => ColumnType::BINARY,
 
         /**
          * Other data types 'cursor' type can't be used with tables
          */
-        'timestamp' => SchemaInterface::TYPE_TIMESTAMP,
-        'hierarchyid' => SchemaInterface::TYPE_STRING,
-        'uniqueidentifier' => SchemaInterface::TYPE_STRING,
-        'sql_variant' => SchemaInterface::TYPE_STRING,
-        'xml' => SchemaInterface::TYPE_STRING,
-        'table' => SchemaInterface::TYPE_STRING,
+        'timestamp' => ColumnType::TIMESTAMP,
+        'hierarchyid' => ColumnType::STRING,
+        'uniqueidentifier' => ColumnType::STRING,
+        'sql_variant' => ColumnType::STRING,
+        'xml' => ColumnType::STRING,
+        'table' => ColumnType::STRING,
     ];
 
     protected function getType(string $dbType, array $info = []): string
     {
-        return self::TYPE_MAP[$dbType] ?? SchemaInterface::TYPE_STRING;
+        return self::TYPE_MAP[$dbType] ?? ColumnType::STRING;
     }
 
     public function fromType(string $type, array $info = []): ColumnSchemaInterface
     {
-        if ($type === SchemaInterface::TYPE_BINARY) {
+        if ($type === ColumnType::BINARY) {
             return (new BinaryColumnSchema($type))->load($info);
         }
 
