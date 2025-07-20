72501679e652b59da219f4959c0d64fbc65e402a
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-07-20 17:22:59 +0300

    adapt

--- src/Builder/LikeConditionBuilder.php
+++ src/Builder/LikeConditionBuilder.php
@@ -10,7 +10,7 @@ use Yiisoft\Db\Expression\ExpressionInterface;
 /**
  * Build an object of {@see \Yiisoft\Db\QueryBuilder\Condition\LikeCondition} into SQL expressions for MSSQL Server.
  */
-final class LikeConditionBuilder extends \Yiisoft\Db\QueryBuilder\Condition\Builder\LikeConditionBuilder
+final class LikeConditionBuilder extends \Yiisoft\Db\QueryBuilder\Condition\LikeConditionBuilder
 {
     /**
      * @var array Map of chars to their replacements in `LIKE` conditions. By default, it's configured to escape
@@ -26,7 +26,7 @@ final class LikeConditionBuilder extends \Yiisoft\Db\QueryBuilder\Condition\Buil
 
     public function build(ExpressionInterface $expression, array &$params = []): string
     {
-        if ($expression->getCaseSensitive() === true) {
+        if ($expression->caseSensitive === true) {
             throw new NotSupportedException('MSSQL doesn\'t support case-sensitive "LIKE" conditions.');
         }
         return parent::build($expression, $params);
